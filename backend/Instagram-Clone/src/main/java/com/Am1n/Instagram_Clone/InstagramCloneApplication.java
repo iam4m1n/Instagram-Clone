062260fc4ee20653f4bb87c2ae781610d1c2383e
Am1n <aminghaedi.mail@gmail.com>
2025-05-21 11:55:44 +0330

    start the backend part and make folder tree structure

---
+++ backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/InstagramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.Am1n.Instagram_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneApplication.class, args);
+	}
+
+}
---
+++ backend/Instagram-Clone/src/test/java/com/Am1n/Instagram_Clone/InstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Am1n.Instagram_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
