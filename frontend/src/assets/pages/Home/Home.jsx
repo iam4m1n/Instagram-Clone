ba6b14ad0260d66dbf49c9a0da0e11f73d0bda62
zibaakord <zibaaakord@gmail.com>
2025-06-05 12:01:52 +0330

    refactor: update Home.jsx to remove LargeNav and prepare for layout-based navigation

--- frontend/src/assets/pages/Home/Home.jsx
+++ frontend/src/assets/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+// src/pages/Home/Home.jsx
+
 import React from "react";
-import { Poutes, Route } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
-      <div className=".w-full h-auto flex items-start justify-between lg:gap-x-32.md:gap-x-16 sm:gap-x-8 gap-x-4 relative"></div>
-    </>
+    <div className="w-full h-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-white">Home Page</h1>
+    </div>
   );
 };
+
+export default Home;
+
