9953a37275cfd26c6afacae6df793b4080b1c230
Am1n <aminghaedi.mail@gmail.com>
2025-06-11 18:29:06 +0330

    add the message entity

---
+++ backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/model/Message/Message.java
@@ -0,0 +1,38 @@
+package com.Am1n.Instagram_Clone.model.Message;
+
+import com.Am1n.Instagram_Clone.model.User.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private UUID messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserEntity sender;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private UserEntity receiver;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    private Instant sentAt = Instant.now();
+
+    @Column(nullable = false)
+    private boolean seen = false;
+}
