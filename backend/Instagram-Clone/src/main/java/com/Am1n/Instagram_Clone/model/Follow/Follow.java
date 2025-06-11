68492e75d6ac15f6f5019bf68d45bf5ba54a6ab1
Am1n <aminghaedi.mail@gmail.com>
2025-06-11 18:27:20 +0330

    create Follow entity

---
+++ backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/model/Follow/Follow.java
@@ -0,0 +1,37 @@
+package com.Am1n.Instagram_Clone.model.Follow;
+
+import com.Am1n.Instagram_Clone.model.User.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "follows",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"follower_id", "followee_id"})
+        }
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Follow {
+
+    @Id
+    @GeneratedValue
+    private UUID followId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private UserEntity follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "followee_id", nullable = false)
+    private UserEntity followee;
+
+    @Column(nullable = false, updatable = false)
+    private Instant followedAt = Instant.now();
+}
