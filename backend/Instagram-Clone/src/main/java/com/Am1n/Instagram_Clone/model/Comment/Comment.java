10da58d6be8011b8ba8f499b5b35563dd2996dad
Am1n <aminghaedi.mail@gmail.com>
2025-06-11 13:52:55 +0330

    create the Commment model

---
+++ backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/model/Comment/Comment.java
@@ -0,0 +1,37 @@
+package com.Am1n.Instagram_Clone.model.Comment;
+
+import com.Am1n.Instagram_Clone.model.Post.Post;
+import com.Am1n.Instagram_Clone.model.User.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private UUID commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String text;
+
+    @CreationTimestamp
+    private Instant timestamp;
+}
