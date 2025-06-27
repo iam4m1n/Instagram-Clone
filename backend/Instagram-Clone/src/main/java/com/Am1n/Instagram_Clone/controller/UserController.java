2b391f33c8796d82b8b5cfd20bd161e7e27a0214
Am1n <aminghaedi.mail@gmail.com>
2025-06-27 21:01:31 +0330

    temp commit

--- backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/controller/UserController.java
+++ backend/Instagram-Clone/src/main/java/com/Am1n/Instagram_Clone/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.Am1n.Instagram_Clone.controller;
 
+import com.Am1n.Instagram_Clone.dto.User.UserDTO;
+import com.Am1n.Instagram_Clone.model.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import com.Am1n.Instagram_Clone.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
 //    Get list of users (search / discover)
     @GetMapping("/")
@@ -35,4 +43,12 @@ public class UserController {
     public String deleteUser(@PathVariable String username){
         return "delete user";
     }
+
+
+    @GetMapping("/all")
+    public Page<UserDTO> getAllUsers(
+            @RequestParam(defaultValue = "0") int page) {
+        return userService.getAllUsers(page);
+    }
+
 }
